0507716d2c368ffa428126864e3b6fcb3c8fc60e
Tomokou <bychckova.margo@yandex.ru>
2023-06-12 13:13:12 +0300

    added calculator

---
+++ CalcTest/Calc.cs
@@ -0,0 +1,7 @@
+﻿using System;
+
+namespace CalcTest;
+public static class Calc
+{
+    public static sum(int a, int b) => a + b;
+}
---
+++ TestConsole/Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+
+using System;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("See ya later"); 
+    }
+}
